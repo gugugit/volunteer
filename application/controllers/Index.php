7ea5b3cf5c3bafb6b060db11e8fac94a77653f82
hiro <buctghlong@163.com>
2017-05-14 23:24:51 +0800

    增加了详情页的显示

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -12,14 +12,21 @@ class IndexController extends Yaf_Controller_Abstract {
      * 首页
     */
     public function indexAction() {//默认Action
-
-//        $this->getView()->assign("content", "你好，我是首页");
         $mActivity = new ActivityModel();
         $datas = $mActivity->query('select * from activity');
-//        var_dump($datas);
         $this->getView()->assign("datas", $datas);
-
-
     }
 
+    /**
+     * 活动详情
+    */
+    public function activitydetailAction($id = 0){
+
+        $mActivity = new ActivityModel();
+        if(!($id = (int)$id) || !$value = $mActivity->fRow($id)){
+            echo "您访问的页面不存在";
+        }
+        $this->getView()->assign("value", $value);
+
+    }
 }
