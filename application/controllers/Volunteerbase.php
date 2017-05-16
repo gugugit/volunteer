119503eb7909be3be634357490e8d4ac17579706
hiro <buctghlong@163.com>
2017-05-17 00:27:36 +0800

    增加啦3d

---
+++ application/controllers/Volunteerbase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricky
+ * Date: 2017/5/16
+ * Time: 23:54
+ */
+class VolunteerbaseController extends Controller_Base{
+
+    public function indexAction(){
+
+    }
+
+}
