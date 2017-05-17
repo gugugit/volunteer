1b0817117cf2990a3010c3562dd4f4e748e30ce7
hiro <buctghlong@163.com>
2017-05-17 13:08:14 +0800

    志愿基地前端显示

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -5,7 +5,7 @@
  * Date: 2017/5/16
  * Time: 23:54
  */
-class VolunteerbaseController extends Controller_Base{
+class BaseController extends Controller_Base{
 
     public function indexAction(){
 
