c88684213c03cd4235e8b81c7c58fee07ceeaf3e
hiro <buctghlong@163.com>
2017-05-17 22:59:04 +0800

    志愿会员模块开发

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -7,8 +7,27 @@
  */
 class BaseController extends Controller_Base{
 
+    /**
+     * 志愿基地首页显示
+    */
     public function indexAction(){
 
+        $model_base = new BaseModel();
+
+        $datas = $this->_list($model_base);
+
+        $this->assign("datas",$datas);
     }
 
+    /**
+     * 基地详情页
+    */
+    public function basedetailAction($id = 0){
+
+        $model_base = new BaseModel();
+        if(!($id = (int)$id) || !$value = $model_base->fRow($id)){
+            echo "您访问的页面不存在";//这里有问题bug。
+        }
+        $this->assign("value",$value);
+    }
 }
---
+++ application/models/Base.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricky
+ * Date: 2017/5/17
+ * Time: 14:58
+ */
+class BaseModel extends Db_Mysql{
+
+    public $pk = 'id';
+
+    public $tablename = 'base';
+}
