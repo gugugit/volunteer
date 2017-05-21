e8c1873e4ab084d52267087234bdccfe3ad97ecf
hiro <buctghlong@163.com>
2017-05-21 12:53:02 +0800

    排名前端网页做好了

---
+++ application/controllers/Rank.php
@@ -0,0 +1,18 @@
+<?php
+class RankController extends Controller_Base{
+
+    /**
+     * 排行榜首页
+    */
+    public function rankingAction(){
+
+
+    }
+
+    /**
+     * 排行榜详情页
+    */
+    public function detaildayAction(){
+
+    }
+}
