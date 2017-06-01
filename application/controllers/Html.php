91acf4db91fede11b47aeee5e54c832a1cd5b7ad
hiro <buctghlong@163.com>
2017-06-01 12:14:13 +0800

    增加了关于我们的界面

---
+++ application/controllers/Html.php
@@ -0,0 +1,32 @@
+<?php
+class HtmlController extends Controller_Base{
+
+    /**
+     * 关于我们
+    */
+    public function  aboutusAction(){
+
+    }
+    /**
+     * 加入我们
+    */
+    public function joinusAction(){
+
+    }
+
+    /**
+     * 联系我们
+     */
+    public function contactusAction(){
+
+    }
+
+
+    /**
+     * 友情链接
+    */
+    public function linksAction(){
+
+    }
+
+}
